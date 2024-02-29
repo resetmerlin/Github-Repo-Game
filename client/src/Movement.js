cb264c5fbbe238d90c1f3dce7c8ec2c6b953517b
resetmerlin <resetmerlin@gmail.com>
2024-02-29 14:02:55 +0900

    refactor: Encapsulate objects of Movement

--- client/src/Movement.js
+++ client/src/Movement.js
@@ -1,85 +1,95 @@
-export class Movement{
+import { Player } from "./Frames";
+
+export class Movement {
+  #boundaries;
+  #player;
+  #movables;
+  #moving;
   constructor({ boundaries, player, movables, moving }) {
-    this.boundaries = boundaries;
-    this.player = player;
-    this.movables = movables;
-    this.moving = moving;
+    this.#boundaries = boundaries;
+    this.#player = player;
+    this.#movables = movables;
+    this.#moving = moving;
   }
 
   moveUp() {
     this.move({ x: 0, y: 3 });
 
-    if (this.moving)
-      this.movables.forEach((movable) => {
-        movable.position.y += 3;
+    if (this.#moving)
+      this.#movables.forEach((movable) => {
+        if (movable?.position) {
+          movable.position.y += 3;
+        } else movable.movePositionY = 3;
       });
   }
 
   moveDown() {
     this.move({ x: 0, y: -3 });
 
-    if (this.moving)
-      this.movables.forEach((movable) => {
-        movable.position.y -= 3;
+    if (this.#moving)
+      this.#movables.forEach((movable) => {
+        if (movable?.position) {
+          movable.position.y -= 3;
+        } else movable.movePositionY = -3;
       });
   }
 
   moveLeft() {
     this.move({ x: 3, y: 0 });
 
-    if (this.moving)
-      this.movables.forEach((movable) => {
-        movable.position.x += 3;
+    if (this.#moving)
+      this.#movables.forEach((movable) => {
+        if (movable?.position) {
+          movable.position.x += 3;
+        } else movable.movePositionX = 3;
       });
   }
 
   moveRight() {
     this.move({ x: -3, y: 0 });
 
-    if (this.moving)
-      this.movables.forEach((movable) => {
-        movable.position.x -= 3;
+    if (this.#moving)
+      this.#movables.forEach((movable) => {
+        if (movable?.position) {
+          movable.position.x -= 3;
+        } else movable.movePositionX = -3;
       });
   }
 
   move({ x = 0, y = 0 }) {
-    for (let i = 0; i < this.boundaries.length; i++) {
-      const boundary = this.boundaries[i];
+    for (let i = 0; i < this.#boundaries.length; i++) {
+      const boundary = this.#boundaries[i];
       const boundaryRectangle = {
         ...boundary,
         position: {
-          x: boundary.position.x + x,
-          y: boundary.position.y + y,
+          x: boundary.boundaryPostionX + x,
+          y: boundary.boundaryPostionY + y,
         },
       };
 
-      if (
-        this.isColliding({
-          playerRectangle: this.player,
-          boundaryRectangle,
-        })
-      ) {
-        moving = false;
+      if (this.isColliding(this.#player, boundaryRectangle)) {
+        this.#moving = false;
         break;
       }
     }
   }
 
-  isColliding({ playerRectangle, boundaryRectangle }) {
+  isColliding(playerRectangle, boundaryRectangle) {
+    if (!(playerRectangle instanceof Player)) return false;
     const IsPlayerTopColliding =
-      playerRectangle.position.y <=
+      playerRectangle.playerPositionY <=
       boundaryRectangle.position.y + boundaryRectangle.height;
 
     const IsPlayerBottomColliding =
-      playerRectangle.position.y + playerRectangle.height >=
+      playerRectangle.playerPositionY + playerRectangle.height >=
       boundaryRectangle.position.y;
 
     const IsPlayerLeftColliding =
-      playerRectangle.position.x + playerRectangle.width >=
+      playerRectangle.playerPositionX + playerRectangle.width >=
       boundaryRectangle.position.x;
 
     const IsPlayerRightColliding =
-      playerRectangle.position.x <=
+      playerRectangle.playerPositionX <=
       boundaryRectangle.position.x + boundaryRectangle.width;
 
     return (
