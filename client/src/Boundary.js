3bfa86c622fa2ea1334829d0f3900c1f215a0f8d
resetmerlin <resetmerlin@gmail.com>
2024-02-29 14:01:38 +0900

    refactor: Encapsulate objects of Boundary

--- client/src/Boundary.js
+++ client/src/Boundary.js
@@ -1,23 +1,38 @@
 export class Boundary {
   static width = 48;
   static height = 48;
+
+  #width;
+  #height;
+  #position;
+  #canvas2d;
+
   constructor({ canvas2d, x, y }) {
-    this.position = {
-      x: x,
-      y: y,
+    this.#position = {
+      x,
+      y,
     };
-    this.width = 48;
-    this.height = 48;
-    this.canvas2d = canvas2d;
+    this.#canvas2d = canvas2d;
+    this.#width = Boundary.width;
+    this.#height = Boundary.height;
   }
 
   draw() {
-    this.canvas2d.fillStyle = "rgba(255,0,0,0.0)";
-    this.canvas2d.fillRect(
-      this.position.x,
-      this.position.y,
-      this.width,
-      this.height
+    this.#canvas2d.fillStyle = "rgba(255,0,0,0.0)";
+
+    this.#canvas2d.fillRect(
+      this.#position.x,
+      this.#position.y,
+      this.#width,
+      this.#height
     );
+  }
+
+  get boundaryPostionX() {
+    return this.#position.x;
+  }
+
+  get boundaryPostionY() {
+    return this.#position.y;
   }
 }
